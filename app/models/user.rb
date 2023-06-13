bd116867722bb1e2f6ac3332c73ed669c8b2ffeb
EC2 Default User <133572109+tatsuya753@users.noreply.github.com>
2023-06-13 00:43:41 +0000

    add バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,9 @@ has_many :books, dependent: :destroy
 
   has_one_attached :profile_image
 
+  validates :name, presence: true, uniqueness: true, length: { in: 2..20 }
+  validates :introduction, length: { maximum: 50 }
+
   def get_profile_image(width, height)
     unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
