2bad3929b3291955ac73243a944497f6d2bab68d
EC2 Default User <133572109+tatsuya753@users.noreply.github.com>
2023-06-14 08:48:44 +0000

    fix ユーザー情報の更新、投稿の更新

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
    if @user.update(user_params)
-    flash[:notice] = "updated book successfully."
+    flash[:notice] = "updated user successfully."
     redirect_to user_path(@user.id)
    else
      render :edit
