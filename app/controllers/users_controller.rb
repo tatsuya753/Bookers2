4a318c901d54c9fe032634b456452f44917eeef3
EC2 Default User <133572109+tatsuya753@users.noreply.github.com>
2023-06-15 08:25:16 +0000

    finish 課題3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-before_action :is_matching_login_user, except: [:index]
+before_action :is_matching_login_user, except: [:index, :show]
 
   def index
     @user = User.all
@@ -34,7 +34,7 @@ before_action :is_matching_login_user, except: [:index]
   def is_matching_login_user
     user = User.find(params[:id])
     unless user.id == current_user.id
-      redirect_to user_path(user.id)
+      redirect_to user_path(current_user.id)
     end
   end
 end
